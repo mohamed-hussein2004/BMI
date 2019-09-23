c3a09ea4dc8cacc95cf42d027b88a015b014b6b5
mohamed hussein <mohamed.hussein.2022@student.lhschools.org>
2019-09-23 09:45:09 -0400

    added a scanner

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,22 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int height;
+        int weight;
+        Scanner person;
+        person = new Scanner(System.in);
+
+
+
+
+
+        
+
+
+
+}
+}
